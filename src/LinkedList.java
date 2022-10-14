b4b3cb4c58ad000495e7da55d7b233d36d7755ec
VincentPierc <vpierce@calpoly.edu>
2022-10-14 16:27:55 -0700

    Linked List tests

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,26 +1,38 @@
 public class LinkedList {
-    Node head = null;
+    private Node head = null;
     static class Node {
-        int val;
-        Node next;
+        private int val;
+        private Node next;
 
-        Node(int val) {
+        public Node(int val) {
             this.val = val;
             next = null;
         }
-
-        public void appendNode(LinkedList list, int val) {
-            Node new_node = new Node(val);
-            if(list.head == null) {
-                list.head = new_node;
-            } else {
-
-            }
-
-
-
-        }
     }
+        public void appendNode(int val) {
+            Node new_node = new Node(val);
+            if(this.head == null) {
+                this.head = new_node;
+            } else {
+                Node current = this.head;
+                while(current.next != null) {
+                    current = current.next;
+                }
+                current.next = new_node;
+            }
+        }
+
+        public String printList() {
+            Node current = this.head;
+            String out = "";
+            if(current == null) { out = "null"; return out; }
+            while(current != null) {
+                out += String.valueOf(current.val);
+                current = current.next;
+            }
+            return out;
+        }
 }
+
 
 
---
+++ src/LinkedList_Tests.java
@@ -0,0 +1,28 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * This file contains test cases for the LinkedList class implementation for the project
+ */
+public class LinkedList_Tests {
+
+    @Test
+    public void testLinkedListConstructor() {
+        LinkedList listy = new LinkedList();
+        listy.appendNode(4);
+        assertEquals("4", listy.printList());
+    }
+
+    @Test
+    public void testLinkedListConstructor2() {
+        LinkedList listy = new LinkedList();
+        listy.appendNode(1);
+        listy.appendNode(2);
+        listy.appendNode(3);
+        assertEquals("123", listy.printList());
+    }
+}
